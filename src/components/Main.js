408d7ede0d93f5dff72c5b73a8bf56f05d4995e1
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2021-06-29 23:52:08 +0300

    Промежуточное сохранение: добавлена возможность лайкать карточки с отправкой/получением данных от сервера

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,35 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import api from './../utils/api.js';
 import Card from './Card.js'
+import { CurrentUserContext } from './../contexts/CurrentUserContext.js'
 
 function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick, ComponentCard }) {
 
-    const [userName, setUserName] = useState('');
-    const [userDescription, setUserDescription] = useState('');
-    const [userAvatar, setUserAvatar] = useState('');
+    const currentUser = React.useContext(CurrentUserContext) // ПОДПИСКА НА КОНТЕКСТ
     const [cards, setCards] = useState([])
+    
 
-
-    // ПОЛУЧЕНИЕ ДАННЫХ ПРОФАЙЛА И ДАННЫХ КАРТОЧКИ ОТ СЕРВЕРА
+    // ПОЛУЧЕНИЕ ДАННЫХ КАРТОЧКИ ОТ СЕРВЕРА
     useEffect(() => {
-
-
-
-        Promise.all([api.getUserInfo(), api.getDataInitialCards()])
+        api.getDataInitialCards()
             .then((result) => {
-
-                const userInfo = result[0];
-                const dataInitialCards = result[1]
-
-                setUserName(userInfo.name);
-                setUserDescription(userInfo.about);
-                setUserAvatar(userInfo.avatar)
-                setCards(dataInitialCards)
+                setCards(result)
             })
-            .catch((err) => {console.log('Ошибка: ', err)})
+            .catch((err) => { console.log('Ошибка: ', err) })
     }, [])
 
 
+    // ОБРАБОТЧИК ЛАЙКА
+    function handleCardLike(card) {
+
+        const isLiked = card.likes.some((profile) => { return profile._id === currentUser._id });
+
+        if (isLiked === true) {
+            api.deleteLikes(card._id)
+
+                .then((targetCard) => {
+                    setCards((cards) => cards.map((c) => c._id === card._id ? targetCard : c))
+                });
+
+        } else {
+            api.plusNumberLikes(card._id)
+                .then((targetCard) => {
+                    setCards((cards) => cards.map((c) => c._id === card._id ? targetCard : c))
+
+                });
+        }
+    }
 
 
     return (
@@ -40,15 +49,15 @@ function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick, ComponentC
 
                     <div className="profile__photo-box">
                         <div onClick={onEditAvatar} className="profile__icon-edit"></div>
-                        <img className="profile__photo" src={userAvatar} alt="Ваше фото" />
+                        <img className="profile__photo" src={currentUser.avatar} alt="Ваше фото" />
                     </div>
 
                     <div className="profile__info">
                         <div className="profile__name-button-box">
-                            <h1 className="profile__name">{userName}</h1>
+                            <h1 className="profile__name">{currentUser.name}</h1>
                             <button onClick={onEditProfile} type="button" className="profile__edit-button"></button>
                         </div>
-                        <p className="profile__status">{userDescription}</p>
+                        <p className="profile__status">{currentUser.about}</p>
                     </div>
                 </div>
 
@@ -57,11 +66,12 @@ function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick, ComponentC
 
             <section className="elements">
                 {/* ИТЕРИРУЕМ ПОЛУЧЕННЫЙ МАССИВ КАРТОЧЕК С СЕРВЕРА
-            ОТПРАВЛЕМ В КОМПОНЕНТ Card В СОСТАВКЕ КАЖДОЙ КАРТОЧКИ НЕОБХОДИМЫЕ ПРОПСЫ
+            ОТПРАВЛЕМ В КОМПОНЕНТ Card В СОСТАВЕ КАЖДОЙ КАРТОЧКИ НЕОБХОДИМЫЕ ПРОПСЫ
             ПРИ СРАБАТЫВАНИИ ОБРАБОТЧИКА onCardClick В Card, ПОЛУЧАЕМ ОТТУДА ДАННЫЕ КАРТОЧКИ
             И ОТПРАВЛЯЕМ ИХ В КОМПОНЕНТ App, ЧТОБЫ ПЕРЕДАТЬ ИХ В КОМПОНЕНТ ImagePopup */}
                 {cards.map((item) => {
-                    return (<Card onCardClick={onCardClick} key={item._id} {...item} />)
+                    {/* console.log(item) */ }
+                    return (<Card onCardLike={handleCardLike} onCardClick={onCardClick} key={item._id} {...item} />)
                 })}
             </section>
 
